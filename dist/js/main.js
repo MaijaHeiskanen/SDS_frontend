e28dfd046672a59a61f0cc94c447c4b774ae3018
Maija <maijaaaw@gmail.com>
2020-02-16 18:42:29 +0200

    Lesson 4 part 1

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,14 +17,14 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add.apply("show"));
+    navItems.forEach(item => item.classList.add("show"));
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove.apply("show"));
+    navItems.forEach(item => item.classList.remove("show"));
     showMenu = false;
   }
 }
